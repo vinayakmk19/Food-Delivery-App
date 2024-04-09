c6c1bf17afb19fa2d9adb1e8a5b5e54ff13f1b98
vinayakmk19 <vinayakmkgen@gmail.com>
2024-04-09 17:10:22 +0530

    Footer few changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,9 +18,9 @@ const Footer = () => {
             possimus quo.
           </p>
           <div className="footer-social-icons">
-            <img src={assets.facebook_icon} alt="" srcset="" />
-            <img src={assets.twitter_icon} alt="" srcset="" />
-            <img src={assets.linkedin_icon} alt="" srcset="" />
+            <img src={assets.facebook_icon} />
+            <img src={assets.twitter_icon} />
+            <img src={assets.linkedin_icon} />
           </div>
         </div>
         <div className="footer-content-center">
