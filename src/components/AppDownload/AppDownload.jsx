ccf737afe777cc355ce9c90d4b54c2fffebb3bdd
vinayakmk19 <vinayakmkgen@gmail.com>
2024-04-18 16:34:00 +0530

    appdownload fix and cart router is created

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { assets } from "../../assets/assets";
+import './appdownload.css';
 
 const AppDownload = () => {
   return (
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,12 +3,14 @@ import "./NavBar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 
-const NavBar = ({setShowLogin}) => {
+const NavBar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
 
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="logo" className="logo" />
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="logo" />
+      </Link>
       <ul className="navbar-menu">
         <Link
           to="/"
@@ -42,10 +44,12 @@ const NavBar = ({setShowLogin}) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
           <div className="dot"></div>
         </div>
-        <button onClick={()=>setShowLogin(true)}>sign in</button>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </div>
   );
